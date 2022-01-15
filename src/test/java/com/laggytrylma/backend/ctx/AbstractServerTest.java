6b4aae31f4ba30c047b80c3811b784a61b94a524
Flowyh <altermydestiny@gmail.com>
2022-01-15 17:48:33 +0100

    Utils/Common true 100% + backend tests

--- src/test/java/com/laggytrylma/backend/ctx/AbstractServerTest.java
+++ src/test/java/com/laggytrylma/backend/ctx/AbstractServerTest.java
@@ -1,9 +1,11 @@
 package com.laggytrylma.backend.ctx;
 
 import com.laggytrylma.helpers.AbstractSystemOutCatch;
+import com.laggytrylma.utils.communication.AbstractSocketBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -13,7 +15,7 @@ import static org.mockito.Mockito.*;
 public class AbstractServerTest extends AbstractSystemOutCatch {
 
   @Test
-  public void testStartServer() throws IOException {
+  public void testStartServer() {
     // Spy class, so we can actually call constructor of Abstract class
     AbstractServer testServer = spy(createServer(21370, "TEST"));
     doNothing().when(testServer).listen();
@@ -31,6 +33,28 @@ public class AbstractServerTest extends AbstractSystemOutCatch {
     assertFalse(testServer.isClosed());
     assertEquals("TEST", testServer.whoAreYou());
     assertEquals(logTime + "[[32mINFO[0m] Running server: TEST on port " + testServer.getPort() + "." + System.lineSeparator(), outContent.toString());
+  }
+
+  @Test
+  public void testStartServerFail() {
+    // Spy class, so we can actually call constructor of Abstract class
+    AbstractServer testServer = spy(createServer(-1, "TEST"));
+    doNothing().when(testServer).listen();
+    when(testServer.whoAreYou()).thenReturn("TEST");
+
+    doCallRealMethod().when(testServer).startServer(100, null);
+    testServer.startServer(100, null);
+
+    LocalDateTime current = LocalDateTime.now();
+    String logTime = "[DATE: " + current.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " TIME: " + current.format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] ";
+    testServer.close();
+    assertEquals(logTime
+                    + "[[31mERROR\u001B[0m] Port value out of range: -1"
+                    + System.lineSeparator()
+                    + logTime
+                    + "[\u001B[31mERROR\u001B[0m] Cannot invoke \"java.net.ServerSocket.close()\" because \"<local2>.serverSocket\" is null"
+                    + System.lineSeparator()
+            , outContent.toString());
   }
 
   @Test
@@ -57,6 +81,26 @@ public class AbstractServerTest extends AbstractSystemOutCatch {
     testServer.startServer(100, null);
     testServer.close();
     assertNotNull(outContent.toString());
+  }
+
+  @Test
+  public void testFlipRunFlag() {
+    AbstractServer test = createServer(21376, "TEST");
+    boolean before = test.running;
+    test.flipRunFlag();
+    boolean after = test.running;
+    assertEquals(!before, after);
+  }
+
+  @Test
+  public void testServerSocketBuilder() throws IOException {
+    AbstractServer test = createServer(21376, "TEST");
+    AbstractSocketBuilder mockBuilder = mock(AbstractSocketBuilder.class);
+    doReturn(mockBuilder).when(mockBuilder).setSocket(any());
+    doReturn(mockBuilder).when(mockBuilder).setupSocket();
+    doReturn(null).when(mockBuilder).build();
+    test.setSocketBuilder(mockBuilder);
+    assertNull(test.createNewSocket(mock(Socket.class)));
   }
 
   public AbstractServer createServer(int port, String name) {
